a7be096c529aa435ddd1ba74cf20a3d577fb0704
mcmrcs@proton.me <marciusdasilva51@gmail.com>
2024-01-21 22:41:08 +0000

    Added statusline

--- .config/nvim/lua/agentengine/packer.lua
+++ .config/nvim/lua/agentengine/packer.lua
@@ -9,7 +9,7 @@ return require('packer').startup(function(use)
     use 'nvim-tree/nvim-tree.lua'
     use 'nvim-tree/nvim-web-devicons'
     use 'wakatime/vim-wakatime'
-
+    use 'nvim-lualine/lualine.nvim'
 
     use {
   	'nvim-telescope/telescope.nvim', tag = '0.1.5',
@@ -24,10 +24,7 @@ return require('packer').startup(function(use)
 	    requires = {
 		    {'williamboman/mason.nvim'},
 		    {'williamboman/mason-lspconfig.nvim'},
-
-		    -- LSP Support
 		    {'neovim/nvim-lspconfig'},
-		    -- Autocompletion
 		    {'hrsh7th/nvim-cmp'},
 		    {'hrsh7th/cmp-nvim-lsp'},
 		    {'L3MON4D3/LuaSnip'},
