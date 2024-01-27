ab88d9fcc2f8fe181b999a5a5e387ac71239812b
mcmrcs@proton.me <marciusdasilva51@gmail.com>
2024-01-27 01:00:33 +0000

    Added rofi config

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -33,4 +33,13 @@ require('mason-lspconfig').setup({
   },
 })
 
+local cmp = require('cmp')
+
+cmp.setup({
+  mapping = cmp.mapping.preset.insert({
+    ['<CR>'] = cmp.mapping.confirm({select = true}),
+    ['<C-Space>'] = cmp.mapping.complete(),
+  })
+})
+
 require('lspconfig').zls.setup {}
--- .config/nvim/lua/agentengine/remap.lua
+++ .config/nvim/lua/agentengine/remap.lua
@@ -1,4 +1,4 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>ww", vim.cmd.w)
-
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
