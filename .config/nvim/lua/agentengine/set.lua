5db27487b3971cf11201df2e67340c7366b9cda4
mcmrcs@proton.me <marciusdasilva51@gmail.com>
2024-01-28 01:22:39 +0000

    nvim settings

--- .config/nvim/lua/agentengine/set.lua
+++ .config/nvim/lua/agentengine/set.lua
@@ -1,3 +1,7 @@
+vim.opt.guicursor = ""
+
+vim.opt.nu = true
+
 vim.opt.relativenumber = true
 
 vim.opt.tabstop = 4
@@ -8,6 +12,8 @@ vim.opt.expandtab = true
 vim.opt.smartindent = true
 
 vim.opt.wrap = false
+
+vim.opt.termguicolors = true
 
 vim.opt.swapfile = false
 vim.opt.backup = false
